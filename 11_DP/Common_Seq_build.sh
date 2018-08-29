09e53e247668de483cc65b06d7ce1e0f29463022
RicciWoo <ricciwoo@gmail.com>
2018-08-29 12:24:29 -0400

    add Longest Common Sequence

---
+++ 11_DP/Common_Seq_build.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 Common_Seq.cpp -o Common_Seq.exe
--- Code_DP.cpp
+++ Code_DP.cpp
@@ -175,6 +175,40 @@ public:
     }
 };
 
+// Longest Increasing Subsequence, Dynamic Programming
+
+
+// Longest Common Sequence, Dynamic Programming
+class Solution {
+public:
+    int longestCommonString(string &a, string &b) {
+        int m = a.size(), n = b.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a[i - 1] == b[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main(int argc, char **argv) {
+    string a = "abcfbc";
+    string b = "abfcab";
+
+    int result;
+    Solution *solution = new Solution;
+    result = solution->longestCommonString(a, b);
+    cout << "string a: " << a << endl;
+    cout << "string b: " << b << endl;
+    cout << "length of longest Common Sequence: " << result << endl;
+}
+
+
 
 
 
