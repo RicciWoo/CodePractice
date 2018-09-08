4d174edae7b964c79de793a55467db1246e50472
RicciWoo <ricciwoo@gmail.com>
2018-09-08 17:23:39 -0400

    add min win substring

---
+++ 14_Hash/Min_Win_Substring.cpp
@@ -0,0 +1,64 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Minimum Window Substring, use Hash Map
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> pattern(256, 0);
+        vector<int> text(256, 0);
+        for (int i = 0; i < t.size(); i++)
+            pattern[t[i]]++;
+        int start = 0, end = 0, minStart = 0, minEnd = 0;
+        int minLen = s.size() + 1;
+        int count = t.size();
+        bool isIncluded = false;
+        text[s[0]]++;
+        if (pattern[s[0]] >= 1) count--;
+        while (true) {
+            if (count == 0) {
+                isIncluded = true;
+                while (text[s[start]] > pattern[s[start]])
+                    text[s[start++]]--;
+                if (minLen > end - start + 1) {
+                    minStart = start;
+                    minEnd = end;
+                    minLen = end - start + 1;
+                }
+            }
+            if (end < s.size() - 1) {
+                text[s[++end]]++;
+                if (pattern[s[end]] >= text[s[end]]) count--;
+            } else break;
+        }
+        if (isIncluded)
+            return s.substr(minStart, minLen);
+        else return "";
+    }
+};
+
+int main(int argc, char **argv) {
+    string s = "abcabcbb";
+    int result;
+    Solution *solution = new Solution;
+    result = solution->lengthOfLongestSubstring(s);
+    cout << "string: \"" << s << "\"" << endl;
+    cout << "max length substring without repating: " << result << endl;
+
+    s = "bbbbb";
+    result = solution->lengthOfLongestSubstring(s);
+    cout << "string: \"" << s << "\"" << endl;
+    cout << "max length substring without repating: " << result << endl;
+
+    s = "pwwkew";
+    result = solution->lengthOfLongestSubstring(s);
+    cout << "string: \"" << s << "\"" << endl;
+    cout << "max length substring without repating: " << result << endl;
+
+    s = "abbcdefdgh";
+    result = solution->lengthOfLongestSubstring(s);
+    cout << "string: \"" << s << "\"" << endl;
+    cout << "max length substring without repating: " << result << endl;
+}
--- Code_Hash.cpp
+++ Code_Hash.cpp
@@ -184,14 +184,38 @@ public:
     }
 };
 
-
-
-
-
-
-
-
-
-
-
-
+// Minimum Window Substring, use Hash Map
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        vector<int> pattern(256, 0);
+        vector<int> text(256, 0);
+        for (int i = 0; i < t.size(); i++)
+            pattern[t[i]]++;
+        int start = 0, end = 0, minStart = 0, minEnd = 0;
+        int minLen = s.size() + 1;
+        int count = t.size();
+        bool isIncluded = false;
+        text[s[0]]++;
+        if (pattern[s[0]] >= 1) count--;
+        while (true) {
+            if (count == 0) {
+                isIncluded = true;
+                while (text[s[start]] > pattern[s[start]])
+                    text[s[start++]]--;
+                if (minLen > end - start + 1) {
+                    minStart = start;
+                    minEnd = end;
+                    minLen = end - start + 1;
+                }
+            }
+            if (end < s.size() - 1) {
+                text[s[++end]]++;
+                if (pattern[s[end]] >= text[s[end]]) count--;
+            } else break;
+        }
+        if (isIncluded)
+            return s.substr(minStart, minLen);
+        else return "";
+    }
+};
