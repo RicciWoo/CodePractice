db251c58534d74a0ff913ca7208d2d1d265ffd2b
RicciWoo <ricciwoo@gmail.com>
2018-09-09 19:01:44 -0400

    update Top N Numbers

--- Code_Heap.cpp
+++ Code_Heap.cpp
@@ -141,7 +141,49 @@ public:
     }
 };
 
-
+// Top N Numbers, use Heap
+class myComp {
+public:
+    bool operator() (const int &a, const int &b) {
+        return a > b;
+    }
+};
+class Solution {
+public:
+    vector<int> topNumbers(vector<vector<int>> &nums) {
+        int m = nums.size();
+        int n = nums[0].size();
+        priority_queue<int, vector<int>, myComp> minHeap;
+        for (int i : nums[0]) minHeap.push(i);
+        vector<int> list(n, 0);
+        for (int i = 1; i < m; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                list[j] = minHeap.top();
+                minHeap.pop();
+            }
+            vector<int> curr = nums[i];
+            sort(curr.begin(), curr.end(), greater<int>());
+            int largest = curr[0];
+            for (int j = 0; j < n; j++)
+                minHeap.push(largest + list[j]);
+            for (int j = 1; j < n; j++) {
+                for (int r = 0; r < n; r++) {
+                    if (curr[j] + list[r] < minHeap.top())
+                        break;
+                    minHeap.pop();
+                    minHeap.push(curr[j] + list[r]);
+                }
+            }
+        }
+        vector<int> result;
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top());
+            minHeap.pop();
+        }
+        sort(result.begin(), result.end(), greater<int>());
+        return result;
+    }
+};
 
 
 
