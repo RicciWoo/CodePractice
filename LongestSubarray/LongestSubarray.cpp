464d827c25472c7c5a885ecdc3dbbbe52434b185
RicciWoo <ricciwoo@gmail.com>
2018-11-11 17:50:20 -0500

    add longest subarray

---
+++ LongestSubarray/LongestSubarray.cpp
@@ -0,0 +1,79 @@
+#include <stack>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+int maxLength(vector<int> &a, int k) {
+    int maxLen = 0;
+    for (int i = 0; i < a.size(); i++) {
+        int sum = 0;
+        for (int j  = i; j < a.size(); j++) {
+            sum += a[j];
+            if (sum <= k) {
+                maxLen = max(maxLen, j - i + 1);
+            }
+        }
+    }
+    return maxLen;
+}
+
+int main(int argc, char **argv) {
+
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string a_count_temp;
+    getline(cin, a_count_temp);
+
+    int a_count = stoi(ltrim(rtrim(a_count_temp)));
+
+    vector<int> a(a_count);
+
+    for (int i = 0; i < a_count; i++) {
+        string a_item_temp;
+        getline(cin, a_item_temp);
+
+        int a_item = stoi(ltrim(rtrim(prices_item_temp)));
+
+        a[i] = a_item;
+    }
+
+    string k_temp;
+    getline(cin, k_temp);
+
+    int k = stoi(ltrim(rtrim(k_temp)));
+
+    int result = maxLength(a, k);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(), 
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
