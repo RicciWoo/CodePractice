19ebcec1847db569c9573319255a10e736201f82
RicciWoo <ricciwoo@gmail.com>
2018-11-11 17:00:18 -0500

    add final disc price

--- FinalDiscountedPrice/FinalDiscPrice.cpp
+++ FinalDiscountedPrice/FinalDiscPrice.cpp
@@ -71,7 +71,7 @@ string ltrim(const string &str) {
 
     s.erase(
         s.begin(), 
-        find_if(s.begin(), s.end(), notl(ptr_fun<int, int>(isspace)))
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
     );
 
     return s;
@@ -81,7 +81,7 @@ string rtrim(const string &str) {
     string s(str);
 
     s.erase(
-        find_if(s.rbegin(), s.rend(), notl(ptr_fun<int, int>(isspace))).base(),
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
         s.begin(), 
     );
 
