fab315a05480dae45704cd907e2ab6d9181fced2
RicciWoo <ricciwoo@gmail.com>
2018-11-23 08:29:49 -0500

    add paindromic substring

--- FindPalindrSubstr_Leetcode647/PalindrSubstr.cpp
+++ FindPalindrSubstr_Leetcode647/PalindrSubstr.cpp
@@ -3,52 +3,46 @@
 #include <algorithm>
 using namespace std;
 
-// Cutting Metal Surplus/Metals, Brute Force, 20181111
+// LeetCode 647 - Palindromic Substrings, Dynamic Programming
 class Solution {
 public:
-    int maxProfit(int cutCost, int unitPrice, vector<int> &lengths) {
-        if (lengths.empty()) {
-            return 0;
+    int countSubstrings(string s) {
+        if (s.size() == 1) {
+            return 1;
         }
-
-        int maxLength = 0;
-        for (int i = 0; i < lengths.size(); i++) {
-            maxLength = max(maxLength, lengths[i]);
+        
+        int n = s.size(), result = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+            result++;
         }
-
-        int maxProfit = 0;
-        for (int i = 1; i <= maxLength; i++) {
-            int profit = 0;
-            for (int j = 0; j < lengths.size(); j++) {
-                int metals = lengths[j] / i;
-                int cuts = metals;
-                if (cuts > 0 && lengths[j] % i == 0) {
-                    cuts--;
-                }
-                profit += metals * i * unitPrice - cuts * cutCost;
-                maxProfit = max(maxProfit, profit);
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                result++;
             }
         }
-
-        return maxProfit;
+        
+        for (int i = n - 3; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                if (dp[i + 1][j - 1] && s[i] == s[j]) {
+                    dp[i][j] = true;
+                    result++;
+                }
+            }
+        }
+        
+        return result;
     }
 };
 
 int main(int argc, char **argv) {
-    int cutCost = 1;
-    int unitPrice = 10;
-    vector<int> lengths{26, 103, 59};
+    string s = "aabaa";
 
     int result;
     Solution *solution = new Solution;
-    result = solution->maxProfit(cutCost, unitPrice, lengths);
-    cout << "cut cost = " << cutCost << endl;
-    cout << "unit price = " << unitPrice << endl;
-    cout << "lengths = [";
-    for (int l : lengths) {
-        cout << l << ", ";
-    }
-    cout << "]" << endl;
-
-    cout << "max profit = " << result << endl;
+    result = solution->countSubstrings(s);
+    cout << "the string s: \"" << s << "\"" << endl;
+    cout << "# of palindromic substring: " << result << endl;
 }
